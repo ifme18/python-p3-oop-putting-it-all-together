68894a150195e0619c19c1be03588863d933e8b6
ifme18 <mainakenken188@gmail.com>
2024-12-03 18:04:46 +0300

    m

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python3
 
-class Book:
-    pass
+class TestBook:
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count=page_count
+    @decorater
+    def title(self):
+        pass
+    @decorater
+    def page_count(self):
+        pass    
+book= TestBook("And Then There Were None", 272)
+print(book.title)
+print(book.page_count)
     
         
