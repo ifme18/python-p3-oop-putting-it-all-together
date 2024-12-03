b088ab90f93de58c799c70494b35ece7a73881a4
ifme18 <mainakenken188@gmail.com>
2024-12-03 18:11:53 +0300

    k

--- lib/book.py
+++ lib/book.py
@@ -9,7 +9,16 @@ class TestBook:
         pass
     @decorater
     def page_count(self):
-        pass    
+     
+        pass 
+    @page_count.setter
+    def page_count(self, value):
+        if isinstance(value, int):
+            self.turn_page = value
+        else:
+            print("page_count must be an intege") 
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")  
 book= TestBook("And Then There Were None", 272)
 print(book.title)
 print(book.page_count)
