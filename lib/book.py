f0bfa1090a81e03b52a96ed50649083e9241af38
ifme18 <mainakenken188@gmail.com>
2024-12-03 18:20:01 +0300

    ok

--- lib/book.py
+++ lib/book.py
@@ -1,27 +1,22 @@
 #!/usr/bin/env python3
 
-class TestBook:
+class Book:
     def __init__(self, title, page_count):
         self.title = title
-        self.page_count=page_count
-        self.page_count = None
-    @decorater
-    def title(self):
-        pass
-    @decorater
+        self._page_count = None
+        self.page_count = page_count  
+    @property
     def page_count(self):
-     
-        pass 
+        return self._page_count
+
     @page_count.setter
     def page_count(self, value):
         if isinstance(value, int):
-            self.turn_page = value
+            self._page_count = value
         else:
-            print("page_count must be an intege") 
+            print("page_count must be an integer")
+
     def turn_page(self):
-        print("Flipping the page...wow, you read fast!")  
-book= TestBook("And Then There Were None", 272)
-print(book.title)
-print(book.page_count)
+        print("Flipping the page...wow, you read fast!")
     
         
