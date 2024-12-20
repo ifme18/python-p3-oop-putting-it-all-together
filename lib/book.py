abaccee60620df2a8915482a291158d05a0ba87e
ken sofware engineer <116180374+ifme18@users.noreply.github.com>
2024-12-20 09:26:08 +0300

    Update book.py

--- lib/book.py
+++ lib/book.py
@@ -1,22 +1,29 @@
-#!/usr/bin/env python3
-
 class Book:
-    def __init__(self, title, page_count):
-        self.title = title
-        self._page_count = None
-        self.page_count = page_count  
-    @property
-    def page_count(self):
-        return self._page_count
+    def __init__(self,title,page_count):
+        self.set_title(title)
+        self.set_page_count(page_count)
 
-    @page_count.setter
-    def page_count(self, value):
-        if isinstance(value, int):
-            self._page_count = value
+    def get_title(self):
+        return self._title 
+    def set_title(self,title):
+        self._title=title
+
+    def get_page_count(self):
+        return self._page_count 
+
+    def set_page_count(self,page_count):
+        if isinstance(page_count,int):
+            self._page_count=page_count
         else:
-            print("page_count must be an integer")
+            print("page_count must be an integer")   
 
     def turn_page(self):
-        print("Flipping the page...wow, you read fast!")
+        print("Flipping the page...wow, you read fast!")         
+
+    title=property(get_title,set_title)
+    page_count=property(get_page_count,set_page_count)          
+        
     
+moby=Book("dick",50)
+moby.turn_page()
         
