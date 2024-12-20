7763d5806bc882542728d0631d314a1774e941db
ken sofware engineer <116180374+ifme18@users.noreply.github.com>
2024-12-20 09:26:48 +0300

    Update shoe.py

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,25 +1,33 @@
-#!/usr/bin/env python3
 
 class Shoe:
-    def __init__(self, brand, size):
-        self.brand=brand
-        self.size= size
-        self.condition="new"
+    def __init__(self,brand,size):
+        self.set_brand(brand)
+        self.set_size(size)
 
-       
-    def cobble(self):
-
-        print("Your shoe is as good as new!")
-        self.condition
-    @decorator
-    def size(self):
-        return self._size
+    def get_brand(self):
+        return self._brand    
         
-    @size.setter
-    def size(self, value):
-        if isinstance(value, int):
-            self._size = value
-        else:
-            print("size must be an integer")
+    def set_brand(self,brand):
+        self._brand=brand
 
+    def get_size(self):
+        return self._size 
+
+    def set_size(self,size):
+        if isinstance(size,int):
+            self._size=size
+        else:
+            print("size must be an integer")  
+
+    def cobble(self,):
+        self.condition="New"
+        print("Your shoe is as good as new!")
+        return "The shoe has been repaired."
+           
+        
+
+    brand=property(get_brand,set_brand)
+    size=property(get_size,set_size)    
+
+adida=Shoe("adida",45)  
         
